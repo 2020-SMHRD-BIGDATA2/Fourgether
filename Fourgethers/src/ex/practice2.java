c85f89e5d9c6746fdedf63e289157d01802e7280
SMHRD <SMHRD@DESKTOP-LD1AV4F>
2020-06-22 15:26:46 +0900

    연습 자바 파일 생성

--- Fourgethers/src/ex/practice2.java
+++ Fourgethers/src/ex/practice2.java
@@ -1,10 +1,10 @@
 package ex;
 
-public class practice1 {
+public class practice2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("sdfsdfsdfasdfsdaf");
+		System.out.println("Asdfsadf");
 	}
 
 }
