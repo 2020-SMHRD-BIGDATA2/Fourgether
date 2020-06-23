424fd5045695dcbe1334eeac242396d279eaaa71
SMHRD <SMHRD@DESKTOP-V5C7O2V>
2020-06-23 17:47:51 +0900

    이미지 재 업로드

--- Fourgethers/src/ex/practice1.java
+++ Fourgethers/src/ex/practice1.java
@@ -5,6 +5,8 @@ public class practice1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sdfsdfsdfasdfsdasdfaasdfsadff");
+		String path = System.getProperty("user.dir");
+		path+="\\src\\";
 	}
 
 }
